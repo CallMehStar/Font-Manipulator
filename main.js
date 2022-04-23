573a988e9a09e25f1b54a25c8abf33e71dffe889
CallMehStar <88277772+CallMehStar@users.noreply.github.com>
2022-04-23 21:00:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,20 +14,29 @@ function setup() {
     poseNet.on('pose', gotposes);
 }
 
-function draw() {
-    background('#969A97');
+function draw(){
+    background("#40419C");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#02011D");
+    text('Sarah',50,400);
 }
 
 function modelDone() {
     console.log("PoseNet Is Initialized And Loaded");
 }
 
-function gotposes(results,error) {
-    if(error) {
+function gotposes(results,error){
+    if(error){
         console.error(error);
     }
-    if(results.length > 0) {
+    if(results.length > 0){
         console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
 
         console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
         console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
